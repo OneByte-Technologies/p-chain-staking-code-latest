dcb0c0a83fe07c5ba8cc021174165a25acbda7d9
akp111 <pradhanashish95@gmail.com>
2023-09-27 18:52:29 +0530

    fixed typos

--- src/interactive-cli.ts
+++ src/interactive-cli.ts
@@ -118,7 +118,7 @@ export async function interactiveCli(baseargv: string[]) {
     else if (walletProperties.wallet == Object.keys(walletConstants)[2] && walletProperties.network && walletProperties.path) {
       // explicitly throw error when ctx.json doesnt exits
       if(!fileExists("ctx.json")){
-        console.error("ctx.json does not exists")
+        console.error("ctx.json does not exist")
         return;
       }
       const amount = await prompts.amount()
