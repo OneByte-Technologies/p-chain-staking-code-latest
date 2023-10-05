e74a7a2224bd501d5ab2b72ca7cffbd5ffbc9f17
akp111 <pradhanashish95@gmail.com>
2023-10-06 03:04:03 +0530

    added check for validator node-id to which delegation was done

--- src/utils.ts
+++ src/utils.ts
@@ -145,9 +145,9 @@ export function integerToDecimal(int: string, offset: number): string {
     return '0'
   }
   int = int.padStart(offset, '0')
-  const part1 = int.slice(0, -offset)
-  const part2 = int.slice(-offset)
-  return part1 + '.' + part2
+  const part1 = int.slice(0, -offset) || "0"
+  const part2 = int.slice(-offset).replace(/0+$/, '');
+  return part1 + (part2 === "" ? "" : '.' + part2)
 }
 
 export function parseRelativeTime(time: string): string {
@@ -168,7 +168,7 @@ export function serializeExportCP_args(args: [BN, string, string, string, string
 
 export function deserializeExportCP_args(serargs: string): [BN, string, string, string, string, string[], number, BN, number, BN?] {
   const args = JSON.parse(serargs)
-  ;[0, 7, 9].map(i => args[i] = new BN(args[i], 16))
+    ;[0, 7, 9].map(i => args[i] = new BN(args[i], 16))
   return args
 }
 
@@ -274,19 +274,25 @@ export function isAlreadySentToChain(id: string): boolean {
   return txObj.isSentToChain ? true : false
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////
+// limiting delegation number
 
-function countpAddressInDelegation(validators: any[], pAddressBech32: string): number {
+export function countpAddressInDelegation(validators: any[], pAddressBech32: string): { count: number, validatorNodeIds: string[] } {
   let count = 0
+  const validatorNodeIds = []
   for (const item of validators) {
     if (item.delegators) {
       for (const delegator of item.delegators) {
-        count += delegator.rewardOwner.addresses.filter((addr: string) => {
-          return addr.toLowerCase() === pAddressBech32.toLowerCase()
-        }).length
+        for (const addr of delegator.rewardOwner.addresses) {
+          if (addr.toLowerCase() === pAddressBech32.toLowerCase()) {
+            count++;
+            validatorNodeIds.push(item.nodeID.toLowerCase())
+          }
+        }
       }
     }
   }
-  return count
+  return { count, validatorNodeIds }
 }
 
 /**
@@ -299,8 +305,19 @@ export async function delegationAddressCount(ctx: Context) {
   const pending = await ctx.pchain.getPendingValidators()
   const pendingValidtaor = JSON.parse(JSON.stringify(pending))
   const pCurrent = JSON.parse(JSON.stringify(current))
-  const count =
-    countpAddressInDelegation(pCurrent.validators, ctx.pAddressBech32!) +
-    countpAddressInDelegation(pendingValidtaor.validators, ctx.pAddressBech32!)
-  return count
+  const currentDelegationDetails = countpAddressInDelegation(pCurrent.validators, ctx.pAddressBech32!)
+  const pendingDelegationDetails = countpAddressInDelegation(pendingValidtaor.validators, ctx.pAddressBech32!)
+  return {count: currentDelegationDetails.count+ pendingDelegationDetails.count, validatorNodeId: [...currentDelegationDetails.validatorNodeIds, ...pendingDelegationDetails.validatorNodeIds]}
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// finalization
+
+export async function waitFinalize<T>(ctx: Context, prms: Promise<T>): Promise<T> {
+  const txcount1 = await ctx.web3.eth.getTransactionCount(ctx.cAddressHex)
+  const resp = await prms
+  while (await ctx.web3.eth.getTransactionCount(ctx.cAddressHex) == txcount1) {
+    await sleepms(1000)
+  }
+  return resp
 }
